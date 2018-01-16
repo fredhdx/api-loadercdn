f3dd0533124d496487f76f22f6fa75d1225336fc
knightReigh <12376953+knightReigh@users.noreply.github.com>
2018-01-15 18:21:35 -0600

    minor sync

--- scripts/meta-transcode.py
+++ scripts/meta-transcode.py
@@ -56,7 +56,7 @@ def read_meta_from_file(mfile):
 
     return meta
 
-def transcode_ffmpeg(ifile,parent_dir, meta, oformat):
+def transcode_ffmpeg(ifile,parent_dir, meta, oformat, overwrite=False):
     if not meta:
         meta = {'title':'','artist':'','album':'','creation_time':'','comment':'', 'year':''}
         print("empty meta")
@@ -65,6 +65,9 @@ def transcode_ffmpeg(ifile,parent_dir, meta, oformat):
         oformat = ifile.split('.')[-1]
 
     try:
+        outputfile = parent_dir + os.path.sep + ifile.split(os.path.sep)[-1].split('.')[0] + "." + oformat
+        if os.path.isfile(outputfile) and not overwrite:
+            return
         command = ("ffmpeg -i \"" + ifile + "\""
                 + " -metadata title=\"" + meta['title']  + "\""
                 + " -metadata artist=\"" + meta['artist'] + "\""
@@ -72,33 +75,38 @@ def transcode_ffmpeg(ifile,parent_dir, meta, oformat):
                 + " -metadata year=\"" + meta['year'] + "\""
                 + " -metadata comment=\"" + meta['creation_time'] + "\""
                 + " -y \""
-                + parent_dir + os.path.sep + ifile.split(os.path.sep)[-1].split('.')[0]
-                + "." + oformat + "\""
+                + outputfile + "\""
                 )
         os.system(command)
     except Exception as e:
         print(e)
         sys.exit(1)
 
-def run_folders(working_dir, oformat="mp3"):
+def run_folders(working_dir, oformat="mp3", overwrite=False):
     # working_dir = os.getcwd() + os.path.sep + "downloaded"
     video_list = [x[:-1] for x in glob(working_dir + "/*/")]
-    try:
-        for video in video_list:
-            """
-            current level: video == /downloaded/video/
-            """
-            info_file = video + os.path.sep + "info.log"
-            media_files = [x for x in glob(video + "/*") if os.path.isfile(x)]
-            media_files = [x for x in media_files if x.split('.')[-1] in supported_format]
+    count = 1
+    for video in video_list:
+        print("PROCESSING %d/%d" % (count,len(video_list)))
+        """
+        current level: video == /downloaded/video/
+        """
+        info_file = video + os.path.sep + "info.log"
+        media_files = [x for x in glob(video + "/*") if os.path.isfile(x)]
+        media_files = [x for x in media_files if x.split('.')[-1] in supported_format]
 
-            for _file in media_files:
-                meta = read_meta_from_file(info_file)
-                # import json
-                # print(json.dumps(meta,indent=4,ensure_ascii=False,sort_keys=True))
-                transcode_ffmpeg(_file, video, meta, oformat)
-    except KeyboardInterrupt:
-        sys.exit()
+        for _file in media_files:
+            meta = read_meta_from_file(info_file)
+            # import json
+            # print(json.dumps(meta,indent=4,ensure_ascii=False,sort_keys=True))
+            transcode_ffmpeg(_file, video, meta, oformat, overwrite)
+
+        count += 1
+
+# index = [1:]
+def get_dirname_by_index(working_dir, position):
+    video_list = [x[:-1] for x in glob(working_dir + "/*/")]
+    return video_list[position-1]
 
 def main():
     # working_dir = os.getcwd() + os.path.sep + "downloaded"
@@ -107,7 +115,7 @@ def main():
         sys.exit()
     else:
         working_dir = sys.argv[1]
-    run_folders(working_dir, "mp3")
+    run_folders(working_dir, "mp3", overwrite=False)
     print("Clean/Done!")
 
 if __name__ == '__main__':
